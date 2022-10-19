e1518c80414313c8b5fd45ac3b56d3fe7e368f17
lucasjk98 <lucasjimenezk1998@gmail.com>
2022-10-19 17:40:46 -0500

    fully functional

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './Box.css'
 
 export const Box = ({value, onClick}) => {
-  const style = value === "X" ? "box x" : value === "TOES"? "box o": "box";
+  const style = value === "X" ? "box x" : value === "O"? "box o": "box";
 
   return (
     <button className = {style} onClick = {onClick}> {value} </button>
